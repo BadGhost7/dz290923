1ab914f8207004a55c62107438b06e39c12d5cfb
skrim <skrim@DESKTOP-B6K33QB>
2023-10-13 16:53:06 +0300

    Добавьте файлы проекта.

---
+++ dz/dz.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Car {
+private:
+    string brand;
+    string model;
+    int year;
+    string color;
+    double price;
+
+public:
+    Car(string brand, string model, int year, string color, double price) {
+        this->brand = brand;
+        this->model = model;
+        this->year = year;
+        this->color = color;
+        this->price = price;
+    }
+
+    void printInfo() {
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+        cout << "Year: " << year << endl;
+        cout << "Color: " << color << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+int main() {
+    Car car("Toyota", "Corolla", 2021, "Black", 25000.50);
+    car.printInfo();
+
+    return 0;
+}
